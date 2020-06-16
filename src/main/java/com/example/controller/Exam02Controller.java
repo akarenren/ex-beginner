4b768640793f338dad080b4b9e85f4c5ad180530
akaseren <ren.akase@rakus.co.jp>
2020-06-16 15:06:27 +0900

    Exam02Controllerのfirstresultにsesseion追加

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -21,6 +21,8 @@ public class Exam02Controller {
 	@RequestMapping("/result1")
 	public String firstResult(int num1, int num2) {
 		int total = num1 + num2;
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
 		session.setAttribute("total", total);
 		
 		return "exam02-result1";
