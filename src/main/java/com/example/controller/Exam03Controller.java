d78c2d9afabbd6552d68a2020f871cf7123faa77
akaseren <ren.akase@rakus.co.jp>
2020-06-16 15:32:27 +0900

    exam3ControllerとProductList作成  Please enter the commit message for your changes. Lines starting

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.form.ProductList;
+
 @Controller
 @RequestMapping("ex03")
 public class Exam03Controller {
@@ -19,7 +21,10 @@ public class Exam03Controller {
 	}
 	
 	@RequestMapping("/result")
-	public String result(Product product) {
+	public String result(ProductList productList) {
+		application.setAttribute("price1", productList.getPrice1());
+		application.setAttribute("price2", productList.getPrice2());
+		application.setAttribute("price3", productList.getPrice3());
 		return "exam03-result";
 	}
 }
