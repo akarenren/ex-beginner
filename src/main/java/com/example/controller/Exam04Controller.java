056a8bbe8013f6fe367251a8d8c3dc1ccfb06625
akaseren <ren.akase@rakus.co.jp>
2020-06-16 16:53:00 +0900

    User追加

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.UserForm;
+
+@Controller
+@RequestMapping("/ex04")
 public class Exam04Controller {
 
+	@Validated UserForm userForm;
+	
 }
