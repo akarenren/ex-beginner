1b6b143354cd2aa9b84f73a108f10ca9d42bb98b
akaseren <ren.akase@rakus.co.jp>
2020-06-16 16:58:02 +0900

    Exam4Controller修正

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -1,15 +1,50 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.domain.User;
 import com.example.form.UserForm;
 
 @Controller
 @RequestMapping("/ex04")
 public class Exam04Controller {
+	
+	@Autowired
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
 
-	@Validated UserForm userForm;
+	@RequestMapping("")
+	public String form() {
+		return "exam04";
+	}
+	
+	@RequestMapping("/result")
+	public String result(
+			@Validated UserForm userForm
+			,BindingResult result
+			,RedirectAttributes redirectAttributes
+			,Model model
+			) {
+		
+		if(result.hasErrors()) {
+			return "exam04";
+		}
+		
+		User user = new User();
+		BeanUtils.copyProperties(userForm, user);
+		
+		model.addAttribute("user", user);
+		
+		return "exam04-result";
+		
+	}
 	
 }
