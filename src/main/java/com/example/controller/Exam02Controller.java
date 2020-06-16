f658c3f99b1c5423d9097835408704875c9b7dc5
akaseren <ren.akase@rakus.co.jp>
2020-06-16 15:00:38 +0900

    exam02-result.html作成

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -1,5 +1,33 @@
 package com.example.controller;
 
-public class Exam02Controller {
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex02")
+public class Exam02Controller {
+	
+	@Autowired
+	private HttpSession session;
+
+	@RequestMapping("")
+	public String form() {
+		return "exam02";
+	}
+	
+	@RequestMapping("/result1")
+	public String firstResult(int num1, int num2) {
+		int total = num1 + num2;
+		session.setAttribute("total", total);
+		
+		return "exam02-result1";
+	}
+	
+	@RequestMapping("/result2")
+	public String secondResult() {
+		return "exam02-result2";
+	}
 }
